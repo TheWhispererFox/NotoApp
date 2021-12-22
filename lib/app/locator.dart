c1f81e8e92f2c5a8c1f6dc73dcbec434322db880
Amir Niyazov <Amir.Niyazov.2001.AN@gmail.com>
2021-12-23 00:44:47 +0300

    Added provider package

--- lib/app/locator.dart
+++ lib/app/locator.dart
@@ -2,11 +2,11 @@ import 'package:get_it/get_it.dart';
 import 'package:injectable/injectable.dart';
 import 'package:noto_app/app/locator.config.dart';
 
-final getIt = GetIt.instance;
+final locator = GetIt.instance;
 
 @InjectableInit(
   initializerName: r'$initGetIt', // default
   preferRelativeImports: true, // default
   asExtension: false, // default
 )
-void configureDependencies() => $initGetIt(getIt);
+void configureDependencies() => $initGetIt(locator);
--- lib/main.dart
+++ lib/main.dart
@@ -3,11 +3,13 @@ import 'package:flutter/material.dart';
 import 'package:noto_app/app/app.dart';
 import 'package:noto_app/app/locator.dart';
 
-Future<void> main() async {
+Future<void> initialize() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-
   configureDependencies();
+}
 
+Future<void> main() async {
+  await initialize();
   runApp(App());
 }
