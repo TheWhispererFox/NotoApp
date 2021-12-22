922dc2d2d61390e6213bbe157f1104819edb5108
Amir Niyazov <Amir.Niyazov.2001.AN@gmail.com>
2021-12-22 23:11:33 +0300

    enable firebase

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,7 @@ import 'package:noto_app/app/themes.dart' as themes;
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  // await Firebase.initializeApp();
+  await Firebase.initializeApp();
 
   runApp(App());
 }
