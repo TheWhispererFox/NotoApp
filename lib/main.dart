e3ea47205e5f71a4f76a1e1c4c993f6743919280
Amir Niyazov <Amir.Niyazov.2001.AN@gmail.com>
2021-12-22 22:32:45 +0300

    Added firebase_core, cloud_firestore

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:noto_app/app/material_auto_router.gr.dart';
 import 'package:noto_app/app/themes.dart' as themes;
 
-void main() => runApp(App());
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+
+  runApp(App());
+}
 
 class App extends StatelessWidget {
   App({Key? key}) : super(key: key);
