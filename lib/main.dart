6d1ed9e6ebf7be83ed395fd5d53fc3ceb8e0386c
Amir Niyazov <Amir.Niyazov.2001.AN@gmail.com>
2021-12-25 12:17:39 +0300

    initialize from firebase_options

--- lib/main.dart
+++ lib/main.dart
@@ -3,10 +3,13 @@ import 'package:flutter/material.dart';
 import 'package:noto_app/app/app.dart';
 import 'package:noto_app/app/locator.dart';
 import 'package:noto_app/app/material_auto_router.gr.dart';
+import 'package:noto_app/firebase_options.dart';
 
 Future<void> initialize() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   locator.registerSingleton<AppRouter>(AppRouter());
   configureDependencies();
 }
