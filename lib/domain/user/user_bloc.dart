ef69a5c90ba544b4efb1846d8ee2f96a5d4d455d
TheWhisperer <houstonplanes@gmail.com>
2021-12-26 14:50:54 +0300

    Add user auth and register methods to User Bloc

--- lib/domain/user/user_bloc.dart
+++ lib/domain/user/user_bloc.dart
@@ -1,14 +1,16 @@
-import 'package:built_collection/built_collection.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:noto_app/app/locator.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 import 'package:noto_app/base/bloc/bloc.dart';
-import 'package:noto_app/data/repositories/firestore/firestore_note_repository.dart';
 import 'package:noto_app/domain/user/user_state.dart';
 
 class UserBloc extends Bloc<UserState, UserStateBuilder> {
   UserBloc(User user) : super(UserState.initial(user));
 
-  final FirestoreNoteRepository _userRepository = locator.get();
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  User? get user {
+    return _auth.currentUser;
+  }
 
   //Stream<BuiltList<Note>> getNotes() => _noteRepository.stream;
   late final events = UserEvents(this);
@@ -19,11 +21,46 @@ class UserEvents {
 
   final UserBloc _bloc;
 
-  void saveNote() {
-    //_bloc._noteRepository.addOrUpdate(_bloc.state.user);
+  Future<void> registerUserWithEmail(String email, String password) async {
+    try {
+      final UserCredential userCredential = await _bloc._auth
+          .createUserWithEmailAndPassword(email: email, password: password);
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'weak-password') {
+        // Notify user that his password is weak
+      } else if (e.code == 'email-already-in-use') {
+        // Notify user that this email already registered
+      }
+    }
   }
 
-  void deleteNote() {
-    //_bloc._noteRepository.delete(_bloc.state.note);
+  Future<void> authUserWithEmail(String email, String password) async {
+    try {
+      final UserCredential userCredential = await _bloc._auth
+          .signInWithEmailAndPassword(email: email, password: password);
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'user-not-found') {
+        // Notify user that this account isn't Found
+      } else if (e.code == 'wrong-password') {
+        // Notify user that they wrote wrong password
+      }
+    }
+  }
+
+  Future<void> signInWithGoogle() async {
+    final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
+    final GoogleSignInAuthentication? googleAuth =
+        await googleUser?.authentication;
+
+    final credential = GoogleAuthProvider.credential(
+      accessToken: googleAuth?.accessToken,
+      idToken: googleAuth?.idToken,
+    );
+
+    await _bloc._auth.signInWithCredential(credential);
+  }
+
+  Future<void> signOut() async {
+    await _bloc._auth.signOut();
   }
 }
