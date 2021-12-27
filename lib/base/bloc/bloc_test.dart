c72406fb0ca362f01035e3e8a5b60196dcdd1ada
Amir Niyazov <Amir.Niyazov.2001.AN@gmail.com>
2021-12-28 01:56:12 +0300

    теперь еще круче, остается нормальный вывод сделать ибо непонятна чаво

--- lib/base/bloc/bloc_test.dart
+++ lib/base/bloc/bloc_test.dart
@@ -18,7 +18,7 @@ void blocTest<
   required List<bool Function(TState state)> statePredicates,
   Future<void> Function(TBloc bloc)? event,
   Duration? wait,
-  int skip = 1,
+  int skip = 0,
   tester.Timeout? timeout,
 }) =>
     tester.test(
